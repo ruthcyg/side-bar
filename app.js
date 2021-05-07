7d4787770faa10e3dfe8ed0bee85347c6cf65593
ruthcyg <76065759+ruthcyg@users.noreply.github.com>
2021-05-08 04:20:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function toggleMenu(){ 			
+    let navigation = document.querySelector('.navigation'); 	
+   let toggleMenu = document.querySelector('.toggle'); 	
+    navigation.classList.toggle('active'); 		
+	toggleMenu.classList.toggle('active'); 	
+	}
+
